9a04d71e4274b5cc1b7993814e593e3791a87dfd
Alex <diamatto2@gmail.com>
2022-08-30 13:09:06 +0400

    revisited bootstrap styling for registration form

--- index.php
+++ index.php
@@ -32,37 +32,36 @@ if ($user) { ?>
     </div>
     <?php
 } else { ?>
-    <body class="h-100 mt-lg-0">
     <link rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
           integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
           crossorigin="anonymous">
-    <div class="bg-image mb-5" style="background-image: url(bg.jpg); height: 100vh">
-        <h1 class="mx-auto" style="width: 250px">Registration</h1>
+    <div class="bg-image" style="background-image: url(bg.jpg); height: 100vh">
+        <h1 class="d-flex justify-content-center">Registration</h1>
         <form method="post" action="do_register.php">
-            <div class="container h-50">
-                <div class="row h-50 justify-content-center align-items-center">
-                    <div class="col-10 col-lg-5">
-                        <div class="form-group" style="max-width: 400px">
-                            <label for="username">Username</label>
-                            <input type="text"
-                                   class="form-control"
-                                   id="username"
-                                   name="username" required
-                                   placeholder="Введите имя пользователя">
-                        </div>
-                        <div class="form-group" style="width: 400px">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password"
-                                   class="form-control"
-                                   id="password"
-                                   name="password" required
-                                   placeholder="Введите пароль">
-                        </div>
-                        <div class="row">
-                            <button type="submit" class="btn btn-primary mr-5 mx-3">Register</button>
-                            <a class="btn btn-outline-primary ml-5" href="login.php">Already have an account?</a>
-                        </div>
+            <div class="container h-50 mt-5" style="width: 400px">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text"
+                           class="form-control"
+                           id="username"
+                           name="username" required
+                           placeholder="Введите имя пользователя">
+                </div>
+                <div class="form-group">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password"
+                           class="form-control"
+                           id="password"
+                           name="password" required
+                           placeholder="Введите пароль">
+                </div>
+                <div class="row justify-content-between">
+                    <div class="btn">
+                        <button type="submit" class="btn btn-primary">Register</button>
+                    </div>
+                    <div class="btn">
+                        <a class="btn btn-outline-primary" href="login.php">Already have an account?</a>
                     </div>
                 </div>
             </div>
